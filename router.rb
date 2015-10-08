49226f1043d308a97793429ece07bd8c512e5c54
doridoridoriand <i.am.eager.for.peace@gmail.com>
2015-10-08 10:03:33 +0900

    add basic routing

--- router.rb
+++ router.rb
@@ -1,1 +1,35 @@
-require 'sinatra'
+require 'bundler'
+Bundler.require
+
+class Router < Sinatra::Base
+
+  configure do
+    set :environment, :production
+    set :port, 3000
+  end
+
+  # トップページ
+  get '/' do
+    erb :index
+  end
+
+  # newsのページ(都道府県個別のページではない)
+  ['/news', '/news/'].map {|route|
+    get route do
+    end
+  }
+
+  # weatherのページ(市町村個別のページではない)
+  ['/weather', '/weather/'].map {|route|
+    get route do
+    end
+  }
+
+  # newsの個別ページ
+  get '/news/:prefecture_id' do
+  end
+
+  # weatherの個別ページ
+  get 'weather/:city_id' do
+  end
+end
