e3411c252b6fd2621283de001718001a1ab2abf8
doridoridoriand <i.am.eager.for.peace@gmail.com>
2015-11-09 01:31:44 +0900

    add constants

---
+++ lib/base.rb
@@ -0,0 +1,5 @@
+module Base
+  API_BASE_URL    = 'http://toa-heavyindustry.net/'
+  ALL_PREFECTURES = 'http://toa-heavyindustry.net/news'
+  ALL_WEATHERS    = 'http://toa-heavyindustry.net/weather'
+end
